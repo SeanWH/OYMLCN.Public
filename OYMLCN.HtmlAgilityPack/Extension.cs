422edd299ef830d1fbfca894345e943cd704b3cc
VicBilibily <mail@oyml.cn>
2017-07-07 10:00:53 +0800

    完善移除方法

--- OYMLCN.HtmlAgilityPack/Extension.cs
+++ OYMLCN.HtmlAgilityPack/Extension.cs
@@ -22,7 +22,7 @@ namespace OYMLCN
         /// </summary>
         /// <param name="html"></param>
         /// <param name="removeComment">移除注释</param>
-        /// <param name="removeScriptAndLinkStyle">移除脚本相关</param>
+        /// <param name="removeScriptAndLinkStyle">移除脚本样式相关</param>
         /// <returns></returns>
         public static HtmlNode AsAgilityHtml(this string html, bool removeComment = true, bool removeScriptAndLinkStyle = true)
         {
@@ -39,41 +39,54 @@ namespace OYMLCN
             {
                 foreach (var script in doc.DocumentNode.Descendants("script").ToArray())
                     script.Remove();
-
                 foreach (var style in doc.DocumentNode.Descendants("style").ToArray())
                     style.Remove();
-
-                foreach (var style in doc.DocumentNode.Descendants("link").ToArray())
+                foreach (var style in doc.DocumentNode.ChildAttributes("style").ToArray())
                     style.Remove();
+                foreach (var link in doc.DocumentNode.Descendants("link").ToArray())
+                    link.Remove();
             }
             return doc.DocumentNode;
+        }
+
+        /// <summary>
+        /// 从Dom中移除符合条件的元素
+        /// </summary>
+        /// <param name="hn"></param>
+        /// <param name="xpath"></param>
+        /// <returns></returns>
+        public static HtmlNode RemoveNode(this HtmlNode hn, string xpath)
+        {
+            foreach (var ele in hn.SelectNodes(xpath).ToArray())
+                ele.Remove();
+            return hn;
         }
 
         /// <summary>
         /// 获取指定路径内元素的Html
         /// </summary>
         /// <param name="hn"></param>
-        /// <param name="domPath"></param>
+        /// <param name="xpath"></param>
         /// <returns></returns>
-        public static string GetInnerHtml(this HtmlNode hn, string domPath) => hn?.SelectSingleNode(domPath)?.InnerHtml?.Trim();
+        public static string GetInnerHtml(this HtmlNode hn, string xpath) => hn?.SelectSingleNode(xpath)?.InnerHtml?.Trim();
         /// <summary>
         /// 获取指定路径内元素的文本
         /// </summary>
         /// <param name="hn"></param>
-        /// <param name="domPath"></param>
+        /// <param name="xpath"></param>
         /// <returns></returns>
-        public static string GetInnerText(this HtmlNode hn, string domPath) => hn?.SelectSingleNode(domPath)?.InnerText?.Trim();
+        public static string GetInnerText(this HtmlNode hn, string xpath) => hn?.SelectSingleNode(xpath)?.InnerText?.Trim();
 
         /// <summary>
         /// 获取指定路径元素的属性值
         /// </summary>
         /// <param name="gn"></param>
-        /// <param name="domPath"></param>
+        /// <param name="xpath"></param>
         /// <param name="attr">属性名称</param>
         /// <param name="def">默认返回值</param>
         /// <returns></returns>
-        public static string GetAttributeValue(this HtmlNode gn, string domPath, string attr, string def) =>
-            gn?.SelectSingleNode(domPath)?.GetAttributeValue(attr, def);
+        public static string GetAttributeValue(this HtmlNode gn, string xpath, string attr, string def) =>
+            gn?.SelectSingleNode(xpath)?.GetAttributeValue(attr, def);
 
 
 
